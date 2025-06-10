7663ed8422e1572b1ee68550b5b045adee763f2f
kavindu919 <jayakodyk2@gmail.com>
2025-06-10 20:06:51 +0530

    complete the finatial flow card

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,38 +1,47 @@
 import { useState } from "react";
 import { MdOutlineArrowDropDownCircle } from "react-icons/md";
-
-const Dropdown = () => {
+interface DropdownItem {
+  value: string;
+  text: string;
+}
+interface DropdownProps {
+  title: string;
+  element: DropdownItem[];
+}
+const Dropdown = ({ title, element }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className="text-primarytext relative w-auto">
+    <div className="text-primarytext relative w-full max-w-48">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className="flex w-full cursor-pointer appearance-none items-center justify-between gap-3 rounded-2xl border-2 border-gray-200 bg-white px-4 py-2 text-base shadow-sm outline-none sm:text-lg"
+        className="flex w-full cursor-pointer appearance-none items-center justify-around gap-3 rounded-2xl border-2 border-gray-200 bg-white px-2 py-1 shadow-sm outline-none"
       >
-        <span>Select Year</span>
+        <span className="text-sm sm:text-base">{title}</span>
         <div className="bg-shade rounded-full md:p-2">
           <MdOutlineArrowDropDownCircle
-            className={`text-theme text-lg transition-transform duration-500 sm:text-xl md:text-2xl ${isOpen ? "rotate-180" : ""}`}
+            className={`text-theme text-lg transition-transform duration-500 sm:text-xl md:text-xl ${isOpen ? "rotate-180" : ""}`}
           />
         </div>
       </button>
 
-      {isOpen && (
-        <ul
-          className={`absolute right-0 left-0 z-10 mt-2 w-full rounded-xl border-2 border-gray-200 bg-white px-4 py-2 text-base shadow-md transition-all duration-200 ease-in-out sm:text-lg ${isOpen ? "pointer-events-auto max-h-screen opacity-100" : "pointer-events-none max-h-0 opacity-0"}`}
-        >
-          <li className="hover:bg-shade cursor-pointer rounded-sm px-2.5 py-1 text-left">
-            2023
+      <ul
+        className={`absolute right-0 left-0 z-10 mt-2 w-full overflow-hidden rounded-xl border-2 border-gray-200 bg-white px-4 py-2 text-sm shadow-md transition-all duration-300 ease-in-out sm:text-base ${
+          isOpen
+            ? "pointer-events-auto max-h-60 opacity-100"
+            : "pointer-events-none max-h-0 opacity-0"
+        }`}
+      >
+        {element.map((ele, index) => (
+          <li
+            key={index}
+            className="hover:bg-shade cursor-pointer rounded-sm px-2.5 py-1 text-left"
+            data-value={ele.value}
+          >
+            {ele.text}
           </li>
-          <li className="hover:bg-shade cursor-pointer rounded-sm px-2.5 py-1 text-left">
-            2023
-          </li>
-          <li className="hover:bg-shade cursor-pointer rounded-sm px-2.5 py-1 text-left">
-            2023
-          </li>
-        </ul>
-      )}
+        ))}
+      </ul>
     </div>
   );
 };
